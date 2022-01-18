e3f37f81cb45f63cff72c4d5366fc08f27538f7d
zainalsalamun <zainalrtf@gmail.com>
2022-01-18 23:44:19 +0700

    init commit

---
+++ lib/main.dart
@@ -0,0 +1,74 @@
+
+import 'package:dicoding_news_app/model/article.dart';
+import 'package:dicoding_news_app/pages/detail_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'News App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      initialRoute: NewsListPage.routeName,
+      routes: {
+        NewsListPage.routeName: (context) => NewsListPage(),
+        ArticleDetailPage.routeName: (context) => ArticleDetailPage(
+          article: ModalRoute.of(context)?.settings.arguments as Article,
+        ),
+        ArticleWebView.routeName: (context) => ArticleWebView(
+          url: ModalRoute.of(context)?.settings.arguments as String,
+        ),
+      },
+    );
+  }
+}
+
+class NewsListPage extends StatelessWidget {
+  static const routeName = '/article_list';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('News App'),
+      ),
+      body: FutureBuilder<String>(
+        future:
+        DefaultAssetBundle.of(context).loadString('assets/articles.json'),
+        builder: (context, snapshot) {
+          final List<Article> articles = parseArticles(snapshot.data);
+          return ListView.builder(
+            itemCount: articles.length,
+            itemBuilder: (context, index) {
+              return _buildArticleItem(context, articles[index]);
+            },
+          );
+        },
+      ),
+    );
+  }
+
+  Widget _buildArticleItem(BuildContext context, Article article) {
+    return ListTile(
+      contentPadding:
+      const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+      leading: Image.network(
+        article.urlToImage,
+        width: 100,
+      ),
+      title: Text(article.title),
+      subtitle: Text(article.author),
+      onTap: () {
+        Navigator.pushNamed(context, ArticleDetailPage.routeName,
+            arguments: article);
+      },
+    );
+  }
+}
